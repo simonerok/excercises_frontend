ad0c3e9d85301031b38b5916776199d1166a0cbd
Simone <ssimone12@gmail.com>
2023-02-03 11:22:31 +0100

    calculator aflevering start

--- basics.js
+++ basics.js
@@ -1,1 +1,14 @@
 console.log("hej");
+/* Denne regel gør at vi går i "strict mode" som betyder at vi får flere errors så vi nemmere kan finde vores fejl fx hvis en variabel ikke er defineret (num = 17;) så vil der i inspectoren i konsollen stå num is not defined. */
+/* Hvordan man tænder for det: skriv "use strict" øverst i dokumentet af js altid*/
+("use strict");
+
+num = 17;
+
+/* test af hvilke værdier der er true og false (2, "2", NaN, Infinity, -0, null {} (array), [] (object) - det skrives i const value efter =) */
+const value = null;
+if (value) {
+  console.log("value is truthy");
+} else {
+  console.log("value is falsy");
+}
