dfd7a2a29d20bc0dde115d868c83fb0bdb086e4a
Simone <ssimone12@gmail.com>
2023-02-09 09:11:49 +0100

    rettet countingArray og begybdt på barChart

---
+++ barChart/barChart.js
@@ -0,0 +1,12 @@
+"use strict";
+
+const queueSize = getNumberOfCustomers();
+
+function getNumberOfCustomers() {
+  /* Faker en kø: giver et komplet tilfældigt tal fra 0-32 */
+  return Math.floor(Math.random() * 32);
+}
+
+/* CALCULATION */
+
+/* DISPLAY */
--- countingArray/count_ny.js
+++ countingArray/count_ny.js
@@ -4,16 +4,16 @@ const arr = [];
 /* iterationer = counter som sættes minus 1 for at den starter på 0 og ikke 2 hvis den var 1 */
 let iterator = -1;
 
-function initLoop() {
+/* kalder funktionen */
+window.addEventListener("DOMContentLoaded", countingLoop);
+
+function countingLoop() {
   iterator++;
-  /* splice fjerner et tal på samme tid med at den sætter et andet ind */
+  /* splice fjerner et tal så den går op til 9 */
   arr.splice(8);
   /* unshift putter noget ind i starten */
   arr.unshift(iterator);
   /* DELAY på 1 sekund 1000 = millisekunder*/
-  setTimeout(initLoop, 1000);
+  setTimeout(countingLoop, 1000);
   console.log(arr);
 }
-
-/* kalder funktionen */
-initLoop();
