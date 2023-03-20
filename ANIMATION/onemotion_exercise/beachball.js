16539f11324062886f91ed8a5e090c256da11a0b
Simone <ssimone12@gmail.com>
2023-03-20 14:52:50 +0100

    multiballs

--- ANIMATION/onemotion_exercise/beachball.js
+++ ANIMATION/onemotion_exercise/beachball.js
@@ -3,7 +3,8 @@
 import { animate } from "https://cdn.skypack.dev/motion";
 
 /* x:400 er hvor langt den bevæger sig */
-/* der er en defalt duration på 0,3, hvis man ønsker en anden skal den sættes*/
+/* der er en defalt duration på 0,3s, hvis man ønsker en anden skal den sættes*/
 /*delay er på 2 sekunder */
 animate(".ball", { x: 400, rotate: 200 }, { duration: 2 });
+/* man skal lave en anden animation når der skal ske to ting på samme tid */
 animate(".ball", { scale: 0 }, { delay: 2 });
--- ANIMATION/onemotion_exercise/multiballs.js
+++ ANIMATION/onemotion_exercise/multiballs.js
@@ -4,4 +4,7 @@ import { animate, stagger } from "https://cdn.skypack.dev/motion";
 /* animate(".ball", { y: -400 }, { duration: 2, repeat: Infinity });
 animate(".ball", { x: 0 }, { delay: 2, stagger: 2 }); */
 
-animate(".ball", { transform: "translateY(calc(-20vw - 21rem))" }, { delay: stagger(0.2), easing: "ease-in-out", duration: 2, repeat: Infinity, direction: "alternate" });
+/* animate(".ball", { transform: "translateY(calc(-20vw - 21rem))" }, { delay: stagger(0.2), easing: "ease-in-out", duration: 2, repeat: Infinity, direction: "alternate" }); */
+
+/* another way of writing it, the first{property object}, and the second{is the options object} */
+animate(".ball", { y: -400 }, { duration: 2, delay: stagger(0.2), repeat: Infinity, direction: "alternate" });
