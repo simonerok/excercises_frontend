9372e9dd3ad830d46e75f30f3b02bfdaa2d1c5b6
Simone <ssimone12@gmail.com>
2023-03-20 14:18:47 +0100

    multiballs øvelse done

--- ANIMATION/onemotion_exercise/beachball.js
+++ ANIMATION/onemotion_exercise/beachball.js
@@ -3,5 +3,7 @@
 import { animate } from "https://cdn.skypack.dev/motion";
 
 /* x:400 er hvor langt den bevæger sig */
-/* duration er tiden: 2 sekunder */
-animate(".ball", { x: 400, rotate: "200deg" }, { duration: 2 });
+/* der er en defalt duration på 0,3, hvis man ønsker en anden skal den sættes*/
+/*delay er på 2 sekunder */
+animate(".ball", { x: 400, rotate: 200 }, { duration: 2 });
+animate(".ball", { scale: 0 }, { delay: 2 });
--- ANIMATION/onemotion_exercise/multiballs.js
+++ ANIMATION/onemotion_exercise/multiballs.js
@@ -1,1 +1,7 @@
 "use strict";
+import { animate, stagger } from "https://cdn.skypack.dev/motion";
+
+/* animate(".ball", { y: -400 }, { duration: 2, repeat: Infinity });
+animate(".ball", { x: 0 }, { delay: 2, stagger: 2 }); */
+
+animate(".ball", { transform: "translateY(calc(-20vw - 21rem))" }, { delay: stagger(0.2), easing: "ease-in-out", duration: 2, repeat: Infinity, direction: "alternate" });
