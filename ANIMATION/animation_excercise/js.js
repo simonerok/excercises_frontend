6355d20442132fb3077c255a515070edc25fa6f8
Simone <ssimone12@gmail.com>
2023-03-20 15:16:40 +0100

    motion øvelse 3 done

--- ANIMATION/animation_excercise/js.js
+++ ANIMATION/animation_excercise/js.js
@@ -1,4 +1,8 @@
-import { animate, stagger } from "https://cdn.skypack.dev/motion";
+import { animate, timeline } from "https://cdn.skypack.dev/motion";
 
 /* transform er brugt når animationen skal ske samme sted */
-animate(".box", { transform: "rotate(45deg)" }, { duration: 1, delay: stagger(0.2) });
+/* animate(".box", { transform: "rotate(45deg)" }, { duration: 1, delay: stagger(0.2) }); */
+
+/* med timeline */
+const myTimeline = timeline([[".box", { rotate: 45 }, { x: 200 }, { duration: 3 }]]);
+/* timeline(myTimeline, options); */
--- ANIMATION/onemotion_exercise/beachball.js
+++ ANIMATION/onemotion_exercise/beachball.js
@@ -1,10 +1,17 @@
 "use strict";
 
-import { animate } from "https://cdn.skypack.dev/motion";
+import { animate, timeline } from "https://cdn.skypack.dev/motion";
 
 /* x:400 er hvor langt den bevæger sig */
 /* der er en defalt duration på 0,3s, hvis man ønsker en anden skal den sættes*/
 /*delay er på 2 sekunder */
-animate(".ball", { x: 400, rotate: 200 }, { duration: 2 });
+
+/* animate(".ball", { x: 400, rotate: 200 }, { duration: 2 }); */
 /* man skal lave en anden animation når der skal ske to ting på samme tid */
-animate(".ball", { scale: 0 }, { delay: 2 });
+/* animate(".ball", { scale: 0 }, { delay: 2 }); */
+
+/* konverteret til timelines */
+myTimeline = timeline([
+  [".ball", { x: 400, rotate: 200 }, { duration: 2 }],
+  [".ball", { scale: 0 }, { delay: 1 }],
+]);
